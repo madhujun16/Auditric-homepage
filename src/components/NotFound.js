d95458505bc099df794ad5fa98e687835b26189e
madhujun16 <madhujun16@gmail.com>
2025-04-17 17:55:29 +0530

    chore: remove reportWebVitals import and usage from index.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   const { t } = useTranslation();
